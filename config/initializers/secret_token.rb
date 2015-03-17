db67e60dd859709608f3bb99ff1edbf0cf1ae89f
ts-zhaorigetu.bao <ts-zhaorigetu.bao@mail.rakuten.com>
2015-03-17 11:42:56 +0900

    [created]depot creted.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'abe827906b9dd3f933eb63a678f92fe504089910d7ad5e97f67e4bc49c523b06cc3695cb1bbde778220f3735e04f7581feb3210335163d73b29b8ce2c56931c8'
