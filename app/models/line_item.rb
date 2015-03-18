ee4168b455e1bdc5b16eb50032d693871ca7e7c1
ts-zhaorigetu.bao <ts-zhaorigetu.bao@mail.rakuten.com>
2015-03-18 12:08:55 +0900

    E

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -6,5 +6,12 @@
 # We make no guarantees that this code is fit for any purpose. 
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
-module ProductsHelper
+class LineItem < ActiveRecord::Base
+  belongs_to :product
+  belongs_to :cart
+  attr_accessible :cart_id, :product_id
+
+  def total_price
+    product.price * quantity
+  end
 end
